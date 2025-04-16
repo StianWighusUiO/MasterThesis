51fd18f843d4ebc2dc422281a48bd891c903e8c7
MisterSwaglagg <shawi99@hotmail.com>
2025-04-16 07:43:16 +0200

    Update Testmodels.R

--- Testmodels.R
+++ Testmodels.R
@@ -12,6 +12,8 @@ library(ggplot2)
 
 set.seed(15052025)
 
+out_of_domain <- FALSE
+
 time_start <- Sys.time()
 
 train <- read_delim("zip.train", delim = " ", col_names = FALSE)
@@ -37,6 +39,12 @@ test <- test %>%
 
 cat("Main data retrival and preprocessing:\t", Sys.time()-time_start, "\n")
 rm(list = c("new_colnames", "time_start"))
+
+if (out_of_domain) {
+  train <- train %>% filter(label != 8)
+  
+}
+
 
 # Naive Bayes
 time_start <- Sys.time()
@@ -142,25 +150,25 @@ cat("Logistic regression, all pariwise interactions:\t", Sys.time() - time_start
 
 # Convergence
 
-# Precompute model matrices for logistic regression
-x_full <- model.matrix(label ~ ., train)[, -1]
-# y_full <- as.numeric(as.character(train$label))
-
 run_models <- function(train) {
-  # test <- test[test_idx, names(train)]
-  
-  
   # Naive Bayes
   nb_structure <- naive.bayes(train, "label")
   nb_model <- bn.fit(nb_structure, train, method = "bayes")
   nb_pred <- predict(nb_model, test)
+  
   acc_NB <- mean(nb_pred == test$label)
+  cm_NB <- confusionMatrix(as.factor(nb_pred), as.factor(test$label))
+  acc_NB <- cm_NB$overall["Accuracy"]
+  class_NB <- cm_NB$byClass[, 1]
   
   # TAN
   TAN_structure <- tree.bayes(train, "label")
   TAN_model <- bn.fit(TAN_structure, train, method = "bayes")
   TAN_pred <- predict(TAN_model, test[, ])
-  acc_TAN <- mean(TAN_pred == test$label)
+  
+  cm_TAN <- confusionMatrix(as.factor(TAN_pred), as.factor(test$label))
+  acc_TAN <- cm_TAN$overall["Accuracy"]
+  class_TAN <- cm_TAN$byClass[, 1]
   
   # Logistic no interactions
   x_logit_train <- model.matrix(as.formula(formula_chr), train)[, -1]
@@ -181,7 +189,10 @@ run_models <- function(train) {
                                  s = logit_cv$lambda.1se,
                                  type = "class"
   )
-  acc_logit <- mean(logit_pred == test$label)
+
+  cm_logit <- confusionMatrix(as.factor(logit_pred), as.factor(test$label))
+  acc_logit <- cm_logit$overall["Accuracy"]
+  class_logit <- cm_logit$byClass[, 1]
   
   
   # Logistic TAN interactions
@@ -208,19 +219,27 @@ run_models <- function(train) {
                                  s = logit_TANinter_cv$lambda.1se,
                                  type = "class"
   )
-  acc_logitTANinter <- mean(logit_TANinter_pred == test$label)
-
   
-  return(c(acc_NB, acc_TAN, acc_logit, acc_logitTANinter))
+  acc_logitTANinter <- mean(logit_TANinter_pred == test$label)
+  cm_logitTANinter <- confusionMatrix(as.factor(logit_TANinter_pred), as.factor(test$label))
+  acc_logitTANinter <- cm_logitTANinter$overall["Accuracy"]
+  class_logitTANinter <- cm_logitTANinter$byClass[, 1]
+  
+  return(data.frame(acc = c(acc_NB, acc_TAN, acc_logit, acc_logitTANinter), 
+                    class = c(class_NB, class_TAN, class_logit, class_logitTANinter)))
 }
 
-n_folds <- 5
-n_resamples <- 3
+n_folds <- 2
+n_resamples <- 2
+
+# data for n_folds = 20, n_resamples = 100
+
 
 set.seed(15052025)
 folds <- vfold_cv(train, v = n_folds, repeats = n_resamples, strata = "label")
 
 results <- data.frame(Model = character(), Fold = integer(), Resample = integer(), Accuracy = numeric())
+class_results <- data.frame(Model = character(), Class = integer(), Fold = integer(), Resample = integer(), Accuracy = numeric())
 
 cl <- makeCluster(detectCores() - 1)
 time_start <- Sys.time()
@@ -229,14 +248,16 @@ for (i in 1:n_resamples) {
   # colnames(train_data) <- colnames(train)
   index <- c()
   
-  for (j in 1:n_folds) {
+  for (j in 1:(n_folds)) {
     if ((j == n_folds) & (i > 1)) {
       next
     }
     new_train_idx <- setdiff(1:length(train$label), folds$splits[[j + (i - 1) * n_folds]][[2]])
     index <- union(index, new_train_idx)
     
-    acc_values <- run_models(train[index, ])
+    from_func <- run_models(train[index, ])
+    acc_values <- from_func["acc"]
+    class_values <- from_func["class"]
     
     temp_results <- data.frame(
       Model = c("NB", "TAN", "Logistic", "Logistic TAN"),
@@ -245,7 +266,16 @@ for (i in 1:n_resamples) {
       Accuracy = acc_values
     )
     
+    class_temp_results <- data.frame(
+      Model = c(rep("NB", 10), rep("TAN", 10), rep("Logistic", 10), rep("Logistic TAN", 10)),
+      Class = rep(0:9, 4),
+      Fold = j,
+      Resample = i,
+      Accuracy = class_values
+    )
+    
     results <- rbind(results, temp_results)
+    class_results <- rbind(class_results, class_temp_results)
     print(paste("Fold", j, "done"))
   }
   print(paste("Resample", i, "done"))
@@ -255,13 +285,17 @@ stopCluster(cl)
 print(results)
 print(Sys.time() - time_start)
 
+
+results <- read_csv("results.csv")
+class_results <- read_csv("class_results.csv")
+
 # Plot convergence
 
 results_summary <- results %>%
   group_by(Model, Fold) %>%
   summarise(
-    Avg_Accuracy = mean(Accuracy),
-    SD = sd(Accuracy),
+    Avg_Accuracy = mean(acc),
+    SD = sd(acc),
     SE = SD / sqrt(n()),
     .groups = "drop"
   )
@@ -269,13 +303,48 @@ results_summary <- results %>%
 ggplot(results_summary, aes(x = Fold, y = Avg_Accuracy, color = Model, group = Model)) +
   geom_line(size = 1) +   
   geom_point(size = 2) +  
-  geom_errorbar(aes(ymin = Avg_Accuracy - SE, ymax = Avg_Accuracy + SE, width = 0.2)) + 
+  geom_errorbar(aes(ymin = Avg_Accuracy - SE, ymax = Avg_Accuracy + SE, width = 0.2)) +
   theme_minimal() +   
   labs(
-    y = "Average Accuracy ?? SE",
+    y = "Average Accuracy with SE",
     color = "Model"
   ) +
   theme(legend.position = "top")
+
+# Plot class convergence for digits
+
+class_results_summary <- class_results %>%
+  group_by(Model, Class, Fold) %>%
+  summarise(
+    Avg_Accuracy = mean(class),
+    SD = sd(class),
+    SE = SD / sqrt(n()),
+    .groups = "drop"
+  )
+
+c_results <- class_results_summary %>%
+  mutate(
+    Fold = as.numeric(Fold),
+    Class = as.factor(Class)
+  )
+
+results_clean <- c_results %>%
+  filter(!is.na(Avg_Accuracy))
+
+# Plot
+ggplot(results_clean, aes(x = Fold, y = Avg_Accuracy, color = Class, group = Class)) +
+  geom_line(size = 1) +
+  geom_point(size = 2) +
+  geom_errorbar(aes(ymin = Avg_Accuracy - SE, ymax = Avg_Accuracy + SE), width = 0.2) +
+  facet_wrap(~ Model, ncol = 2) +
+  theme_minimal() +
+  labs(
+    title = "Per-Class Accuracy Over Folds",
+    x = "Fold",
+    y = "Average Accuracy with SE",
+    color = "Class"
+  ) +
+  theme(legend.position = "bottom")
 
 
 # Plot numbers
